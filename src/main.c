603f4761cc9de3833bcd239bdc8e75826696d0ea
Milo Niemaz <mniemaz@z3r9p6.42lyon.fr>
2025-04-11 15:20:48 +0200

    main

--- src/main.c
+++ src/main.c
@@ -6,23 +6,23 @@
 /*   By: mniemaz <mniemaz@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:00:00 by mniemaz           #+#    #+#             */
-/*   Updated: 2025/04/11 15:17:53 by mniemaz          ###   ########.fr       */
+/*   Updated: 2025/04/11 15:20:34 by mniemaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// int main(int argc, char **argv, char **envp)
-// {
-//     (void)argc;
-//     (void)argv;
+int main(int argc, char **argv, char **envp)
+{
+    (void)argc;
+    (void)argv;
 
-//     if (!envp || !*envp)
-//     {
-//         write(STDERR_FILENO, "Error: No environment variables provided.\n", 42);
-//         return (EXIT_FAILURE);
-//     }
+    if (!envp || !*envp)
+    {
+        write(STDERR_FILENO, "Error: No environment variables provided.\n", 42);
+        return (EXIT_FAILURE);
+    }
 
-//     minishell(envp);
-//     return (EXIT_SUCCESS);
-// }
+    minishell(envp);
+    return (EXIT_SUCCESS);
+}
