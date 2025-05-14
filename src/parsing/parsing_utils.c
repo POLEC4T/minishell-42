528c3f3508d27d01c5aa012c0f72fb4fe7deeabe
ShadowSheep <nathan@torgan.net>
2025-05-14 11:12:13 +0200

    handle single quote

--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: nle-gued <nle-gued@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 10:55:33 by nle-gued          #+#    #+#             */
-/*   Updated: 2025/05/14 10:47:30 by nle-gued         ###   ########.fr       */
+/*   Updated: 2025/05/14 11:10:27 by nle-gued         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,15 @@ size_t	skip_redirection(char *str, size_t i)
 
 size_t	skip_word(char *str, size_t i)
 {
-	int		in_quotes;
+	char		in_quotes;
 
 	in_quotes = 0;
 	while (str[i])
 	{
-		if (str[i] == '"' && in_quotes) // Fin des guillemets
+		if (str[i] == in_quotes && in_quotes) // Fin des guillemets
 			in_quotes = 0;
-		else if (str[i] == '"' && !in_quotes) // Début des guillemets
-			in_quotes = 1;
+		else if ((str[i] == '"' || str[i] == '\'') && !in_quotes) // Début des guillemets
+			in_quotes = str[i];
 		if (!in_quotes && (str[i] == ' ' || str[i] == '<' || str[i] == '>')) // Hors des guillemets, arrêter
 			break;
 		else // Inclure les caractères valides
