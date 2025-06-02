f975260035373edc2c6c4f9fddacf84b682a1d0e
Milo Niemaz <mniemaz@z3r9p4.42lyon.fr>
2025-06-02 14:01:15 +0200

    fix: leak line read

--- src/parsing/maintest.c
+++ src/parsing/maintest.c
@@ -6,7 +6,7 @@
 /*   By: mniemaz <mniemaz@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 07:47:13 by nle-gued          #+#    #+#             */
-/*   Updated: 2025/05/29 20:12:36 by mniemaz          ###   ########.fr       */
+/*   Updated: 2025/06/02 13:47:05 by mniemaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,9 @@ int	handle_error_feedback(t_context *ctx)
 		if (g_signal > 0)
 		{
 			free_ctx_cmds(ctx);
+			init_ctx_cmds(ctx);
+			free(ctx->rl_str);
+			ctx->rl_str = NULL;
 			return (EXIT_SUCCESS);
 		}
 		ctx->exit_code = EXIT_FAILURE;
@@ -65,36 +68,32 @@ int	handle_error_feedback(t_context *ctx)
 
 t_context	*read_cmds(t_context *ctx)
 {
-	char	*read;
-
 	while (1)
 	{
 		g_signal = 0;
 		setup_parent_signals(ctx);
-		read = readline("pitishell$ ");
+		ctx->rl_str = readline("pitishell$ ");
 		if (g_signal == SIGINT)
 			ctx->exit_code = 128 + g_signal;
-		if (!read)
+		if (!ctx->rl_str)
 		{
 			write(1, "exit\n", 6);
 			exit_free(ctx);
 		}
-		add_history(read);
-		if (is_syntax_valid(read) == EXIT_FAILURE)
-			free(read);
-		else
-		{
-			read = expand_line(read, ctx, CMD);
-			if (parsing_init(read, ctx) == EXIT_FAILURE)
-				if (handle_error_feedback(ctx) == EXIT_SUCCESS)
-					continue ;
-			ft_exec(ctx);
-			free_ctx_cmds(ctx);
-			free_exec(ctx->exec_data);
-			clean_init_exec(ctx);
-			free(read);
-		}
+		add_history(ctx->rl_str);
+		if (!is_syntax_valid(ctx->rl_str))
+			continue ;
+		ctx->rl_str = expand_line(ctx->rl_str, ctx, CMD);
+		if (parsing(ctx->rl_str, ctx) == EXIT_FAILURE)
+			if (handle_error_feedback(ctx) == EXIT_SUCCESS)
+				continue ;
+		ft_exec(ctx);
+		free_ctx_cmds(ctx);
+		init_ctx_cmds(ctx);
+		free(ctx->rl_str);
+		ctx->rl_str = NULL;
+		free_exec(ctx->exec_data);
+		clean_init_exec(ctx);
 	}
-	free(read);
 	return (NULL);
 }
